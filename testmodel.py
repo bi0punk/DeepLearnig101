218d2fdcfab6fb853fa92857bebe3eef152cde11
bi0punk <systecinfo_cl@criptext.com>
2023-06-26 23:37:22 -0400

    reset

--- testmodel.py
+++ testmodel.py
@@ -12,12 +12,19 @@ y_train_encoded = np.array([label_mapping[label] for label in y_train])
 
 
 tokenizer = tf.keras.preprocessing.text.Tokenizer()
+
 x_train_encoded = tokenizer.fit_on_texts(x_train)
+
 x_train_encoded = tokenizer.texts_to_sequences(x_train)
+
 max_seq_length = len(max(x_train_encoded, key=len))
+
 x_train_padded = pad_sequences(x_train_encoded, maxlen=max_seq_length)
+
 num_classes = len(label_mapping)
+
 y_train_encoded = tf.keras.utils.to_categorical(y_train_encoded, num_classes=num_classes)
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Embedding(len(tokenizer.word_index) + 1, 32, input_length=max_seq_length),
     tf.keras.layers.GlobalAveragePooling1D(),
@@ -44,10 +51,3 @@ decoded_predictions = [list(label_mapping.keys())[label] for label in np.argmax(
 
 for input_text, prediction in zip(filtered_x_test, decoded_predictions):
     print(f'Input: {input_text}, Predicted Output: {prediction}')
-
-
-    """ El método fit_on_texts ahora se llama antes de la conversión a secuencias para aprovechar la tokenización y la construcción del vocabulario al mismo tiempo.
-
-    La variable max_seq_length se calcula utilizando len(max(x_train_encoded, key=len)) para obtener la longitud máxima de secuencia en el conjunto de entrenamiento directamente.
-
-    En el bucle de impresión, se utiliza zip para iterar simultáneamente sobre filtered_x_test y decoded_predictions, evitando la necesidad de usar el índice i. """
