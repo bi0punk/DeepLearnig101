fc185936bd1e70aa2d70c3fadcda0f0b4058e0db
bi0punk <systecinfo_cl@criptext.com>
2023-07-06 03:33:37 -0400

    clean

--- testmodel.py
+++ testmodel.py
@@ -3,11 +3,11 @@ import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Datos de entrenamiento
-x_train = np.array(['Hola', 'Hola, ¿cómo estás?', 'Buen día', 'Saludos'], dtype=object)
-y_train = np.array(['Hola', 'Bien, ¿y tú?', 'Hola', 'Hola'], dtype=object)
+x_train = np.array(['Hola', 'Hola, ¿cómo estás?', 'Buen día'], dtype=object)
+y_train = np.array(['Hola Señor', 'Hola', 'Hola'], dtype=object)
 
 # Mapeo de etiquetas
-label_mapping = {'Hola': 0, 'Bien, ¿y tú?': 1, 'Otra clase': 2}  # Agrega todas las clases posibles
+label_mapping = {'Hola': 0, 'Hola Señor': 1, 'Otra clase': 2}  # Agrega todas las clases posibles
 y_train_encoded = np.array([label_mapping[label] for label in y_train])
 
 # Tokenización
@@ -17,32 +17,31 @@ x_train_encoded = tokenizer.texts_to_sequences(x_train)
 max_seq_length = len(max(x_train_encoded, key=len))
 x_train_padded = pad_sequences(x_train_encoded, maxlen=max_seq_length)
 
-# Codificación de etiquetas
-num_classes = len(label_mapping)
-y_train_encoded = tf.keras.utils.to_categorical(y_train_encoded, num_classes=num_classes)
-
 # Definición del modelo
 model = tf.keras.models.Sequential([
     tf.keras.layers.Embedding(len(tokenizer.word_index) + 1, 32, input_length=max_seq_length),
     tf.keras.layers.GlobalAveragePooling1D(),
     tf.keras.layers.Dense(16, activation='relu'),
-    tf.keras.layers.Dense(num_classes, activation='softmax')
+    tf.keras.layers.Dense(len(label_mapping), activation='softmax')
 ])
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train_padded, y_train_encoded, epochs=100, verbose=0)
 
-# Datos de prueba
-print("")
-x_test = np.array([input("Ingrese el texto de prueba: ")])
-x_test_encoded = tokenizer.texts_to_sequences(x_test)
-filtered_x_test = [x_test[i] for i, sequence in enumerate(x_test_encoded) if any(sequence)]
-filtered_x_test_encoded = [sequence for sequence in x_test_encoded if any(sequence)]
-filtered_x_test_padded = pad_sequences(filtered_x_test_encoded, maxlen=max_seq_length)
+while True:
+    # Datos de prueba
+    print("")
+    x_test = np.array([input("Ingrese el texto de prueba: ")])
+    x_test_encoded = tokenizer.texts_to_sequences(x_test)
+    x_test_padded = pad_sequences(x_test_encoded, maxlen=max_seq_length)
 
-# Predicciones
-predictions = model.predict(filtered_x_test_padded)
-decoded_predictions = [list(label_mapping.keys())[label] for label in np.argmax(predictions, axis=1)]
+    # Predicciones
+    predictions = model.predict(x_test_padded)
+    decoded_predictions = [list(label_mapping.keys())[np.argmax(pred)] for pred in predictions]
 
-# Resultados
-for input_text, prediction in zip(filtered_x_test, decoded_predictions):
-    print(f'Input: {input_text}, Predicted Output: {prediction}')
+    # Resultados
+    for input_text, prediction in zip(x_test, decoded_predictions):
+        print(f'Input: {input_text}, Predicted Output: {prediction}')
+
+    choice = input("¿Deseas probar otro dato? (s/n): ")
+    if choice.lower() != "s":
+        break
